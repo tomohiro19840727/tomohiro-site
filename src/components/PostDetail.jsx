4a2bcae6d8a7e9170badbfe0ecc7d929aabc4d19
tomohiro <tomohirofarm@gmail.com>
2023-05-15 13:31:57 +0900

    修正

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -9,16 +9,18 @@ SyntaxHighlighter.registerLanguage('javascript', javascript);
 
 function PostDetail({ selectedPostText, selectedCode }) {
   return (
-    <div className="flex flex-col items-center justify-center py-4 bg-gray-100">
-      <p className="text-xl text-gray-800 mb-4">{selectedPostText}</p>
-      <div className="bg-gray-900 rounded-lg overflow-hidden max-w-xl">
-        <SyntaxHighlighter
-          language="javascript"
-          style={materialDark}
-          className="p-4 text-sm font-mono text-white"
-        >
-          {selectedCode}
-        </SyntaxHighlighter>
+    <div className="flex flex-col items-center justify-center py-4 bg-gray-100 min-h-screen">
+      <div className="container mx-auto">
+        <p className="text-2xl text-gray-800 mb-4">{selectedPostText}</p>
+        <div className="bg-gray-900 rounded-lg overflow-hidden">
+          <SyntaxHighlighter
+            language="javascript"
+            style={materialDark}
+            className="p-4 text-sm font-mono text-white"
+          >
+            {selectedCode}
+          </SyntaxHighlighter>
+        </div>
       </div>
     </div>
   );
