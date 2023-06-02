35160059c99c1f0016240271b0ea671ff97c647d
tomohiro <tomohirofarm@gmail.com>
2023-06-02 16:36:22 +0900

    メール送信OK

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,21 @@
+const {onRequest} = require("firebase-functions");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const sgMail = require("@sendgrid/mail");
-const cors = require("cors")({origin: true});
+const cors = require("cors")({ origin: true });
 
-const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = functions.config().sendgrid.apikey;
 
 admin.initializeApp();
 sgMail.setApiKey(apiKey);
 
 exports.sendMail = functions.https.onRequest((req, res) => {
-  // CORS設定を適用する
   cors(req, res, async () => {
-    const {name, email, message} = req.body;
+    const { name, email, message } = req.body;
 
     const msg = {
-      to: "tomohirofarm@gmail.com", // 送信先のメールアドレス
-      from: "tomohiro-site@gmail.com", // 送信元のメールアドレス
+      to: "tomohirofarm@gmail.com",
+      from: "tomohiro-site.com",
       subject: "お問い合わせ",
       text: `名前: ${name}\nメールアドレス: ${email}\nメッセージ: ${message}`,
     };
@@ -23,10 +23,10 @@ exports.sendMail = functions.https.onRequest((req, res) => {
     try {
       await sgMail.send(msg);
       console.log("メールが送信されました");
-      res.json({message: "メールが送信されました。"});
+      res.json({ message: "メールが送信されました。" });
     } catch (error) {
       console.error("メールの送信に失敗しました:", error);
-      res.status(500).json({error: "メールの送信に失敗しました。"});
+      res.status(500).json({ error: "メールの送信に失敗しました。" });
     }
   });
 });
