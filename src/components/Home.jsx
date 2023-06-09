f379f3ba19f67852184708c431dc05b48c587156
tomohiro <tomohirofarm@gmail.com>
2023-06-09 13:30:38 +0900

    見た目

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,50 +38,78 @@ function Home({setSelectedPostText, selectedPostText, setSelectedCode}) {
      <div class="relative w-screen h-screen">
   <video class="absolute top-0 left-0 w-full h-full object-cover z-0" src={videoBg} autoPlay muted loop playsInline/>
   <div class="absolute bottom-60 right-12 transform translate-x-1/14 translate-y-1/2 z-10">
-    <h1 class="text-white text-6xl font-bold mb-4 sm:text-7xl">Tomohiro</h1>
-    <p class="text-white text-6xl font-bold mb-4 sm:text-7xl">Kuriki</p>
+    <h1 class="text-white text-6xl font-bold mb-4 sm:text-7xl animate-delayed-tracking-in-expand">Tomohiro</h1>
+    <p class="text-white text-6xl font-bold mb-4 sm:text-7xl animate-delayed-tracking-in-expand">Kuriki</p>
   </div>
   <div class="absolute mt-40 left-12 transform -translate-y-1/2 z-10 w-1/2">
-    <h1 class=" text-7xl sm:text-9xl font-bold mb-4 text-indigo-100">Welcome</h1>
-    <p class=" text-5xl sm:text-7xl font-bold mb-4 text-indigo-100">to My Site</p>
+    <h1 class=" text-7xl sm:text-9xl font-bold mb-4 text-indigo-100 animate-delayed-tracking-in-expand">Welcome</h1>
+    <p class=" text-5xl sm:text-7xl font-bold mb-4 text-indigo-100 animate-delayed-tracking-in-expand">to My Site</p>
   </div>
 </div>
 
 
 
-<section className='text-gray-700 border-t border-gray-200 bg-white py-6 sm:py-8 lg:py-12 pb-6 sm:pb-8 lg:pb-12 bg-h-screen w-full bg-gradient-to-br from-violet-300 via-blue-500 to-orange-100'>
-      <div className='container px-5 py-24 mx-auto'>
-        <div className='text-center md-20 mb-20'>
-          <h1 className='text-2xl sm:text-3xl mb-2 text-gray-900 font-bold'>最新の投稿情報</h1>
+  <h3 className="text-4xl font-bold m-20 flex justify-center">About me</h3>
+
+    <div className="about flex flex-wrap justify-center items-center">
+    <div className="w-full md:w-48 h-48 rounded-full overflow-hidden flex-shrink-0 md:mr-8">
+      <img src="./img/20220227-A7401987_TP_V4.jpg" alt="" className="w-full h-full object-cover" />
+    </div>
+  </div>
+
+
+  <div className="flex justify-center m-20">
+  <div className="flex items-center w-full h-auto">
+    <div className="w-1/2 h-full bg-cover bg-center opacity-50" style={{ backgroundImage: "url('./img/スクリーンショット 2023-06-09 10.22.26.png')" }}></div>
+    <div className="w-1/2 text-black text-center p-8 text-2xl mt-8 font-bold animate-tracking-in-expand">
+      <p className="leading-tight"><br />未経験から競走馬の牧場で10年・・<br /><br />
+      競走馬を生産、育成、馴致等で<br/><br/>
+      日本一の牧場である「ノーザンファーム」で躍進！<br /><br /></p>
+    </div>
+  </div>
+</div>
+
+  <div className="flex justify-center m-20">
+  <div className="flex items-center w-full h-auto">
+    <div className="w-1/2 text-black text-center p-8 text-2xl mt-8 font-bold">
+    <p><br /><br />「農業」でも未経験ながら6年・・<br/><br/>
+    実践的な経験を積み重ねました<br /><br /></p>
+      <p>トラクター、コンバインなど、様々な大型機械にも習熟！<br /><br /></p>
+    </div>
+    <div className="w-1/2 h-full bg-cover bg-center opacity-80" style={{ backgroundImage: "url('./img/スクリーンショット 2023-06-09 10.41.42.png')" }}></div>
+  </div>
+</div>
+
+<div className="flex justify-center m-20">
+  <div className="flex items-center w-full h-auto">
+    <div className="w-1/2 h-full bg-cover bg-center opacity-50" style={{ backgroundImage: "url('./img/スクリーンショット 2023-06-09 11.54.52.png')" }}></div>
+    <div className="w-1/2 text-black text-center p-8 text-2xl mt-8 font-bold">
+    <div className="text-black text-center p-8 text-2xl mt-8 font-bold">
+        コールセンターのオペレーターとして<br/>
+        コミュニケーション能力を培う<br /><br />
+        
+        その後、オペレーターの指揮を執る<br/>SV（スーパーバイザー）へと転身<br /><br />
+        顧客の期待を超えるサービスを提供し、<br/>的確な解決策をお届けします!<br /><br />
         </div>
-        <div className='flex flex-wrap -mx-4'>
-          {sortedLists.map((post) => (
-            <div key={post.id} className='md:w-1/3 px-4 mb-8'>
-              <div className='bg-gray-200 rounded-lg p-8'>
-                <div className='flex items-center mb-3'>
-                  <div className='bg-green-500 text-white rounded-full h-8 w-8 flex items-center justify-center'></div>
-                  <span className='text-gray-800 text-xl font-bold block'>
-                    {dayjs.unix(Number(post.createdAt)).tz('Asia/Tokyo').format('MM/DD HH:mm')}
-                  </span>
-                  <h2 className='text-gray-900 text-lg font-bold ml-2'>{post.title}</h2>
-                </div>
-                <Link
-                  to='/postdetail'
-                  onClick={() => handleClick(post)}
-                  className='flex mt-3 text-green-500 items-center font-bold hover:text-green-600 transition-colors duration-300'
-                >
-                  詳細
-                </Link>
-              </div>
-            </div>
-          ))}
+    </div>
+  </div>
+</div>
+
+<div className="flex justify-center">
+  <div className="flex items-center w-full h-auto">
+  <div className="text-black text-center p-8 text-2xl mt-8 font-bold m-20">
+        「 独学 」でエンジニアに転身し,<br/>着実にスキルを磨いています。<br /><br />
+      『 React.js、Typescript、TailwindCSS等 』で<br/>魅力的なアプリを開発中！<br /><br />
+      PHP、AWS、Next.jsにもチャレンジ予定！<br /><br />
         </div>
-      </div>
-    </section>
+    <div className="w-1/2 h-full bg-cover bg-center opacity-80 ml-10" style={{ backgroundImage: "url('./img/21830a0I9A97521937_TP_V4.jpg')" }}></div>
+  </div>
+</div>
 
 
 
-   <section className='text-gray-700 border-t border-gray-200 bg-white py-6 sm:py-8 lg:py-12 bg-h-screen w-full bg-gradient-to-br
+
+<section className='text-gray-700 border-t border-gray-200 bg-white py-6 sm:py-8 lg:py-12 bg-h-screen w-full bg-gradient-to-br
   from-violet-300 via-blue-100 to-orange-100'>
     <div className='container px-5 py-24 mx-auto flex flex-wrap'>
       <div className='mb-10 lg:mb-0 w-full lg:w-1/2 flex justify-center'>
@@ -114,7 +142,40 @@ function Home({setSelectedPostText, selectedPostText, setSelectedCode}) {
     </div>
 
    </section>
+
+   <section className='text-gray-700 border-t border-gray-200 bg-white py-6 sm:py-8 lg:py-12 pb-6 sm:pb-8 lg:pb-12 bg-h-screen w-full bg-gradient-to-br from-violet-300 via-blue-500 to-orange-100'>
+      <div className='container px-5 py-24 mx-auto'>
+        <div className='text-center md-20 mb-20'>
+          <h1 className='text-2xl sm:text-3xl mb-2 text-gray-900 font-bold'>最新の投稿情報</h1>
+        </div>
+        <div className='flex flex-wrap -mx-4'>
+          {sortedLists.map((post) => (
+            <div key={post.id} className='md:w-1/3 px-4 mb-8'>
+              <div className='bg-gray-200 rounded-lg p-8'>
+                <div className='flex items-center mb-3'>
+                  <div className='bg-green-500 text-white rounded-full h-8 w-8 flex items-center justify-center'></div>
+                  <span className='text-gray-800 text-xl font-bold block'>
+                    {dayjs.unix(Number(post.createdAt)).tz('Asia/Tokyo').format('MM/DD HH:mm')}
+                  </span>
+                  <h2 className='text-gray-900 text-lg font-bold ml-2'>{post.title}</h2>
+                </div>
+                <Link
+                  to='/postdetail'
+                  onClick={() => handleClick(post)}
+                  className='flex mt-3 text-green-500 items-center font-bold hover:text-green-600 transition-colors duration-300'
+                >
+                  詳細
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+
                         </>
+
+                        
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "delayed-tracking-in-expand": "delayed-tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.5s both"
+      },
+      keyframes: {
+        "delayed-tracking-in-expand": {
+          "0%": {
+            "letter-spacing": "-.5em",
+            opacity: "0"
+          },
+          "40%": {
+            opacity: ".6"
+          },
+          to: {
+            opacity: "1"
+          }
+        }
+      }
+    },
+
+    
   },
   plugins: [],
 }
